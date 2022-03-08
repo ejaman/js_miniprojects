78fa5b31ba1d00cfe3bb7e6d9da159d56517c9dc
ejaman <leegm042@gmail.com>
2022-03-09 01:10:29 +0900

    handle brush size & color

--- paintingCanvas/app.js
+++ paintingCanvas/app.js
@@ -1,8 +1,15 @@
 const canvas = document.getElementById("jsCanvas");
 const context = canvas.getContext("2d");
+const color = document.getElementsByClassName("controlColor");
+const range = document.querySelector("#jsRange");
+const fillBtn = document.querySelector("#jsMode");
+const sizeX = document.querySelector("#jsX");
+const sizeY = document.querySelector("#jsY");
 
-context.strokeStyle = "black";
-context.lineWidth = 2.5;
+canvas.width = 1000;
+canvas.height = 600;
+context.lineWidth = 10.1;
+context.lineCap = "round";
 let painting = false;
 
 function stopPaint() {
@@ -13,24 +20,33 @@ function startPaint() {
   painting = true;
 }
 
+function OnColorChange(event) {
+  const bgColor = event.target.style.backgroundColor;
+  context.strokeStyle = bgColor;
+}
+function onHandleRange(event) {
+  const range = event.target.value;
+  console.log(range);
+  context.lineWidth = range;
+}
+function onHandleFill() {
+  context.fillStyle = context.strokeStyle;
+  context.fillRect(0, 0, 1000, 600);
+}
+function onHandleSize(event) {
+  console.log(event);
+}
+
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
   if (!painting) {
-    context.beginPath();
+    context.beginPath(); // line
     context.moveTo(x, y);
   } else {
     context.lineTo(x, y);
     context.stroke();
   }
-}
-
-// function onMouseDown(event) {
-//   painting = true;
-// }
-
-function onMouseUp() {
-  stopPaint();
 }
 
 if (canvas) {
@@ -39,3 +55,7 @@ if (canvas) {
   canvas.addEventListener("mouseup", stopPaint);
   canvas.addEventListener("mouseleave", stopPaint);
 }
+Array.from(color).forEach((c) => c.addEventListener("click", OnColorChange));
+range.addEventListener("input", onHandleRange);
+fillBtn.addEventListener("click", onHandleFill);
+sizeX.addEventListener("input", onHandleSize);
